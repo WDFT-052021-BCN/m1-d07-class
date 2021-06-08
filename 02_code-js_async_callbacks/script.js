9254e66e194e00a6859a7085c6f568b27ed49046
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-08 15:32:35 +0200

    Done set timeout

--- 02_code-js_async_callbacks/script.js
+++ 02_code-js_async_callbacks/script.js
@@ -6,15 +6,28 @@ let toggleButton = document.getElementById("hide-div");
 
 dropdownSection.classList.add("open");
 
+const onClick = () => {
+  console.log("You clicked the button");
+  setTimeout(() => {
+    let text;
+    if (toggleButton.innerText === "Hide") {
+      text = "Show";
+    } else {
+      text = "Hide";
+    }
+    toggleButton.innerText = text;
+    dropdownSection.classList.toggle("close");
+    console.log("Time is up");
+  }, 3000);
+};
+
 toggleButton.addEventListener("click", onClick);
 
-function onClick(event) {
-  let text;
-  if (this.innerText === "Hide") {
-    text = "Show";
-  } else {
-    text = "Hide";
-  }
-  this.innerText = text;
-  dropdownSection.classList.toggle("close");
-}
+console.log("Some button has been clicked");
+setTimeout(() => {
+  // Doing a lot of crazy thing before time is up ....
+  // Doing a lot of crazy thing before time is up ....
+  // Doing a lot of crazy thing before time is up ....
+  console.log("Time is up");
+}, 1000);
+console.log("Timer is running");
